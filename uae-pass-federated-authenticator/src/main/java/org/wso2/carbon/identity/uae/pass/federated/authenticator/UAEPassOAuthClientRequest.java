38733b58771ca86b14b6f6ab1ba5f718c353c7ab
dinuwan97 <kalubowila.dc25132@live.com>
2022-01-04 15:26:30 +0530

    Added the acr values property as a query string

--- uae-pass-federated-authenticator/src/main/java/org/wso2/carbon/identity/uae/pass/federated/authenticator/UAEPassOAuthClientRequest.java
+++ uae-pass-federated-authenticator/src/main/java/org/wso2/carbon/identity/uae/pass/federated/authenticator/UAEPassOAuthClientRequest.java
@@ -4,6 +4,7 @@ import org.apache.oltu.oauth2.client.request.ClientHeaderParametersApplier;
 import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
 import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
 import org.apache.oltu.oauth2.common.message.OAuthMessage;
+import org.apache.oltu.oauth2.common.message.types.GrantType;
 import org.apache.oltu.oauth2.common.parameters.BodyURLEncodedParametersApplier;
 import org.apache.oltu.oauth2.common.parameters.OAuthParametersApplier;
 import org.apache.oltu.oauth2.common.parameters.QueryParameterApplier;
@@ -25,16 +26,6 @@ public class UAEPassOAuthClientRequest extends OAuthClientRequest{
         return new UAEPassOAuthClientRequest.AuthenticationRequestBuilder(url);
     }
 
-    public static class TokenRequestBuilder extends UAEPassQueryParams.OAuthRequestBuilder {
-        public TokenRequestBuilder(String url) {
-            super(url);
-        }
-
-        public UAEPassOAuthClientRequest.TokenRequestBuilder setUiLocales(String ui_locales) {
-            this.parameters.put("ui_locales",ui_locales);
-            return this;
-        }
-    }
 
     public static class AuthenticationRequestBuilder extends UAEPassOAuthClientRequest.OAuthRequestBuilder {
         public AuthenticationRequestBuilder(String url) {
@@ -53,6 +44,11 @@ public class UAEPassOAuthClientRequest extends OAuthClientRequest{
 
         public UAEPassOAuthClientRequest.AuthenticationRequestBuilder setUiLocales(String ui_locales){
             this.parameters.put("ui_locales",ui_locales);
+            return this;
+        }
+
+        public UAEPassOAuthClientRequest.AuthenticationRequestBuilder setAcrValues(String acr_values){
+            this.parameters.put("acr_values",acr_values);
             return this;
         }
 
@@ -76,6 +72,79 @@ public class UAEPassOAuthClientRequest extends OAuthClientRequest{
             return this;
         }
     }
+
+
+    public static class TokenRequestBuilder extends UAEPassOAuthClientRequest.OAuthRequestBuilder {
+        public TokenRequestBuilder(String url) {
+            super(url);
+        }
+
+        public UAEPassOAuthClientRequest.TokenRequestBuilder setUiLocales(String ui_locales) {
+            this.parameters.put("ui_locales",ui_locales);
+            return this;
+        }
+
+        public UAEPassOAuthClientRequest.TokenRequestBuilder setGrantType(GrantType grantType) {
+            this.parameters.put("grant_type", grantType == null ? null : grantType.toString());
+            return this;
+        }
+
+        public UAEPassOAuthClientRequest.TokenRequestBuilder setClientId(String clientId) {
+            this.parameters.put("client_id", clientId);
+            return this;
+        }
+
+        public UAEPassOAuthClientRequest.TokenRequestBuilder setClientSecret(String secret) {
+            this.parameters.put("client_secret", secret);
+            return this;
+        }
+
+        public UAEPassOAuthClientRequest.TokenRequestBuilder setUsername(String username) {
+            this.parameters.put("username", username);
+            return this;
+        }
+
+        public UAEPassOAuthClientRequest.TokenRequestBuilder setPassword(String password) {
+            this.parameters.put("password", password);
+            return this;
+        }
+
+        public UAEPassOAuthClientRequest.TokenRequestBuilder setScope(String scope) {
+            this.parameters.put("scope", scope);
+            return this;
+        }
+
+        public UAEPassOAuthClientRequest.TokenRequestBuilder setCode(String code) {
+            this.parameters.put("code", code);
+            return this;
+        }
+
+        public UAEPassOAuthClientRequest.TokenRequestBuilder setRedirectURI(String uri) {
+            this.parameters.put("redirect_uri", uri);
+            return this;
+        }
+
+        public UAEPassOAuthClientRequest.TokenRequestBuilder setAssertion(String assertion) {
+            this.parameters.put("assertion", assertion);
+            return this;
+        }
+
+        public UAEPassOAuthClientRequest.TokenRequestBuilder setAssertionType(String assertionType) {
+            this.parameters.put("assertion_type", assertionType);
+            return this;
+        }
+
+        public UAEPassOAuthClientRequest.TokenRequestBuilder setRefreshToken(String token) {
+            this.parameters.put("refresh_token", token);
+            return this;
+        }
+
+        public UAEPassOAuthClientRequest.TokenRequestBuilder setParameter(String paramName, String paramValue) {
+            this.parameters.put(paramName, paramValue);
+            return this;
+        }
+    }
+
 
     public abstract static class OAuthRequestBuilder {
         protected OAuthParametersApplier applier;
