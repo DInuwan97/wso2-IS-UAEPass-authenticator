7f419dba86d2fc1995a3288e825bde4ef878c60b
dinuwan97 <kalubowila.dc25132@live.com>
2022-01-19 00:27:45 +0530

    Constants move to util package

---
+++ uae-pass-federated-authenticator/src/main/java/org/wso2/carbon/identity/uae/pass/federated/authenticator/util/UAEPassAuthenticatorConstants.java
@@ -0,0 +1,26 @@
+package org.wso2.carbon.identity.uae.pass.federated.authenticator.util;
+
+public class UAEPassAuthenticatorConstants {
+    public static final String LOGIN_TYPE = "OIDC";
+
+    public static final String OAUTH_OIDC_SCOPE = "openid";
+    public static final String OAUTH2_GRANT_TYPE_CODE = "code";
+    public static final String OAUTH2_PARAM_STATE = "state";
+
+    public static final String ACCESS_TOKEN = "access_token";
+    public static final String ID_TOKEN = "id_token";
+
+    public static final String CLIENT_ID = "ClientId";
+    public static final String CLIENT_SECRET = "ClientSecret";
+    public static final String CALLBACK_URL = "callbackUrl";
+    public static final String OAUTH2_AUTHZ_URL = "OAuth2AuthzEPUrl";
+    public static final String OAUTH2_TOKEN_URL = "OAuth2TokenEPUrl";
+
+    public static final String HTTP_ORIGIN_HEADER = "Origin";
+    public static final String SUB = "sub";
+
+    public static final String UI_LOCALES = "ui_locales";
+    public static final String ACR_VALUES = "acr_values";
+
+    public static final String OIDC_DIALECT = "http://wso2.org/oidc/claim";
+}
